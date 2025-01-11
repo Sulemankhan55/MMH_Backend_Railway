bf443ed6512b98f9c378b1005c0b2baba9501e6e
tekisky <151526222+Sulemankhan55@users.noreply.github.com>
2025-01-11 17:03:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,42 @@
-import express  from "express";
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./db/connectDB.js";
-import patientRouter from './routers/patientRoute.js'
-import userRouter from './routers/userRouter.js'
+import patientRouter from "./routers/patientRoute.js";
+import userRouter from "./routers/userRouter.js";
 import morgan from "morgan";
 import dashboardRoute from "./routers/dashboardRoute.js";
 
-//connection of mongodb
-connectDB()
 
-dotenv.config()
-const app = express()
-const PORT = process.env.PORT || 5000
+dotenv.config();
 
-//Express Middlewares
+
+connectDB();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// CORS Configuration
+const corsOptions = {
+  origin: [
+    "http://localhost:3000", // Add your frontend domain(s) here
+    "https://mmh.tekisky.com"
+  ],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // Allow credentials (cookies, Authorization headers, etc.)
+};
+
+// Express Middlewares
 app.use(express.json());
+app.use(cors(corsOptions));
+app.use(morgan("dev"));
 
-app.use(cors())
-app.use(morgan('dev'))
+// Routes
+app.use("/patient", patientRouter);
+app.use("/user", userRouter);
+app.use("/mmh", dashboardRoute);
 
-
-//Routes
-app.use('/patient',patientRouter)
-app.use('/user',userRouter)
-app.use('/mmh',dashboardRoute)
-
-
-
-app.listen(PORT , ()=>{
-    console.log(`listening on ${PORT}`);
-}) 
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
